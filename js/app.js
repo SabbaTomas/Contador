e804a57c91da7cb5536cb5a4f789025726ce6c39
Tomas Sabbavini <tomassabbavini@hotmail.com>
2022-06-22 00:58:51 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
-let numero = document.getElementById("contador_numero_principal");
+let numero = document.getElementById("seleccion");
 let contadores = document.getElementById("contador_main");
 let cantidadDeContadores = document.getElementsByClassName("nums");
 
 let contador = 0;
+
+
+
+const cambiarContador = () => {
+    console.log("hola");
+}
 
 const incrementar = () => {
   actualizarContador(++contador);
@@ -23,18 +29,18 @@ const agregarContador = () => {
   if (cantidadDeContadores.length < 3) {
     const h3 = document.createElement("h3");
     h3.className = "nums";
-    h3.setAttribute("id", "contador_numero_secundario");
     h3.innerHTML = 0;
     contadores.appendChild(h3);
   }
 };
 
 const quitarContador = () => {
-    contadores.lastChild.remove();
+    if(cantidadDeContadores.length > 1) {
+        contadores.lastChild.remove();
+    }
 };
-
-
 
 const actualizarContador = (num) => {
-  numero.innerHTML = num;
+  seleccion.innerHTML = num;
 };
+
